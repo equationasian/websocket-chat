648eaa18798067aba18bf15e03897f42f4f44f00
Anna Nguyen <1110nguyena@gmail.com>
2024-01-09 19:47:44 -0600

    Authentication with Spring Security

--- src/main/java/com/example/Chat/WebSecurityConfig.java
+++ src/main/java/com/example/Chat/WebSecurityConfig.java
@@ -2,8 +2,11 @@ package com.example.Chat;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -17,17 +20,20 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(requests ->
-                requests.requestMatchers("/", "/css/**", "/js/**")
+                requests.requestMatchers("/css/**", "/js/**", "/signup.html")
                         .permitAll()
                         .anyRequest()
                         .authenticated())
-            .formLogin(form -> form.loginPage("/login.html").permitAll());
+            .formLogin(form -> form.loginPage("/login.html").permitAll().defaultSuccessUrl("/index.html", true))
+            .logout(LogoutConfigurer::permitAll)
+            .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user")
+        UserDetails user = User.builder()
+                .username("user")
                 .password(bCryptPasswordEncoder().encode("password"))
                 .roles("USER")
                 .build();
