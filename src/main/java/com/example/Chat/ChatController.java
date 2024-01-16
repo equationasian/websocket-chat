8f3d960fc81ef2c618ab053de9b71f1fa62a75c3
Anna Nguyen <1110nguyena@gmail.com>
2024-01-16 01:08:49 -0600

    Fixed broadcast msg on new client connections

---
+++ src/main/java/com/example/Chat/ChatController.java
@@ -0,0 +1,4 @@
+package com.example.Chat;
+
+public class ChatController {
+}
--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -1,8 +1,8 @@
 const signupForm = document.getElementById("login-form");
 const successParam = new URLSearchParams(window.location.search);
 if (successParam.has("registered")) {
-    const errorMessage = signupForm.insertAdjacentHTML("beforebegin", "<div id='success-message'>Successfully registered</div>")
+    const errorMessage = signupForm.insertAdjacentHTML("beforebegin", "<div id='success-container'><span id='success-message'><i class='fa-solid fa-check'></i>Successfully registered</span></div>");
 }
 else if (successParam.has("error")) {
-    const errorMessage = signupForm.insertAdjacentHTML("beforebegin", "<div id='error-message'>Invalid username or password</div>")
+    const errorMessage = signupForm.insertAdjacentHTML("beforebegin", "<div id='success-container'><span id='error-message'><i class='fa-solid fa-xmark'></i>Invalid username or password</span></div>");
 }
